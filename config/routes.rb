37bfc64bb7b08f7d73b2442e46d620382407bd53
grey <permale.rey@gmail.com>
2022-04-24 13:20:49 +1000

    Created seeds file for sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
   get 'pages/home'
+
   get 'pages/about'
+
   get 'pages/contact'
+  
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
